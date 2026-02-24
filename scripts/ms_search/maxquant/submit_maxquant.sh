19af9414166477b654547aa450cc91ff21a70b67
Andrew Leduc <leduc.an@northeastern.edu>
2026-02-24 12:05:18 -0500

    maxQuant

--- scripts/ms_search/maxquant/submit_maxquant.sh
+++ scripts/ms_search/maxquant/submit_maxquant.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
 #SBATCH --time=24:00:00
 #SBATCH --partition=slavov
 #SBATCH --job-name=maxquant
